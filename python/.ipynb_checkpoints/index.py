72bd59fdb85589b65b7a832cd0d0e74349576169
SHARVAN223 <sharvan70458@gmail.com>
2025-08-30 09:32:31 +0530

    argemitic opertor

---
+++ python/.ipynb_checkpoints/index.py
@@ -0,0 +1,21 @@
+# a = int(input("enter a number: "))
+# b = int(input("enter a number: "))
+# print("this value is:",a+b)
+
+# b = int (input("enter a number:"))
+# h = int (input("enter a number:"))
+# print("area of triangle:",b*h/3.14)
+
+
+# l = int (input("enter a number:"))
+# w = int (input("enter a number:"))
+# print("area of rectangle:",l*w)
+
+
+r = int (input("enter a number:"))
+print("area of circle:",3.14*r*r)
+
+
+
+
+
